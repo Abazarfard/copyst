6f920e33f0ff9aecd15ca3cbd94a61f061695426
Abazarfard <91885900+Abazarfard@users.noreply.github.com>
2025-01-23 17:21:03 +0330

    Delete App directory

--- App/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-
-export default defineConfig({
-  plugins: [svelte()],
-  base: 'https://github.com/Abazarfard/copyst.git', // Replace 'your-repo-name' with your repository name
-});
